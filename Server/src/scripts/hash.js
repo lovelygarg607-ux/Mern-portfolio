b07c4442c26d467f89414768c7d4dacf0abe75f6
lovelygarg607-ux <lovelygarg607@gmail.com>
2026-01-18 21:43:24 +0530

    Update hash.js

--- Server/src/scripts/hash.js
+++ Server/src/scripts/hash.js
@@ -1,10 +1,1 @@
-import bcrypt from "bcrypt";
 
-const password = "1234"
-
-const createHash = async () => {
-  const hash = await bcrypt.hash(password, 10);
-  console.log("Hashed Password:", hash);
-};
-
-createHash();
