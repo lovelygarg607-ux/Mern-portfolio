9fd5c4a15ae784529c37b2d9048b6a232cc9dcf1
lovelygarg607-ux <lovelygarg607@gmail.com>
2026-01-21 21:31:05 +0530

    Update projectcontroller.js

--- Server/src/controllers/projectcontroller.js
+++ Server/src/controllers/projectcontroller.js
@@ -93,8 +93,8 @@ const addprojectcontroller = async (req, res) => {
 
 const getprojectlist = async (req, res) => {
   try {
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 3;
+    const page = Number(req.query.page) 
+    const limit = Number(req.query.limit) || 10;
 
     const pageskip = (page - 1) * limit;
 
